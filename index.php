333a0d6a7f23bf1a738976e5b58d63da83d31285
Millasanches55 <106831415+Millasanches55@users.noreply.github.com>
2025-05-16 17:14:13 -0300

    atualização

--- index.php
+++ index.php
@@ -4,11 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-<<<<<<< HEAD
-    <title>TCC</title>
-=======
-    <title>Home</title>
->>>>>>> 88b4316a85f956f0c87d373ac0d547fea5b51ffd
+    <title>Document</title>
 </head>
 <body>
     <header>
