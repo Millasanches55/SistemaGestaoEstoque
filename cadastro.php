9d392feaa650b5cba54a28974b67bd01eed9e582
Kamilly-Barreto <kamilly.sousab@gmail.com>
2025-05-16 17:11:22 -0300

    atualização

--- cadastro.php
+++ cadastro.php
@@ -6,6 +6,30 @@
     <title>Cadastro</title>
 </head>
 <body>
-    
+    <header>
+        <h1>Sistema de TCC</h1>
+    </header>
+
+    <?php
+        $tipo_cadastro = $_POST['opcao-cadastro'];
+
+        switch($tipo_cadastro) {
+            case "Aluno":
+                
+                break;
+            
+            case "Professor":
+            
+                break;
+        
+            case "Agenda":
+            
+                break;
+
+            case "TCC":
+            
+                break;
+        }
+    ?>
 </body>
 </html>
